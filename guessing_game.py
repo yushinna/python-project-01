b2554149e9d507aadc41b8d6f1d68d3326f20d03
yushinna <yuminnakao@Yumin-Nakao.local>
2019-05-07 14:21:20 +0900

    Fix file
    Fix the following issues
    - higher and lower reversed
    - not to count improper input as a try

--- guessing_game.py
+++ guessing_game.py
@@ -3,7 +3,7 @@ Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
 
-For this first project we will be using Workspaces. 
+For this first project we will be using Workspaces.
 
 NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
 
@@ -14,7 +14,7 @@ import random
 
 def start_game(highscore=999):
     """Psuedo-code Hints
-    
+
     When the program starts, we want to:
     ------------------------------------
     1. Display an intro/welcome message to the player.
@@ -22,33 +22,33 @@ def start_game(highscore=999):
     3. Continuously prompt the player for a guess.
       a. If the guess greater than the solution, display to the player "It's lower".
       b. If the guess is less than the solution, display to the player "It's higher".
-    
+
     4. Once the guess is correct, stop looping, inform the user they "Got it"
          and show how many attempts it took them to get the correct number.
     5. Let the player know the game is ending, or something that indicates the game is over.
-    
+
     ( You can add more features/enhancements if you'd like to. )
     """
     # write your code inside this function.
-    
+
     answer = random.randint(1, 10)
     tries = 0
     highscore = highscore
-    
-    while True:  
+
+    while True:
         try:
             guess = int(input("Pick a number between 1 and 10: "))
-            tries += 1
             if guess < 1 or guess > 10:
                 raise ValueError
         except ValueError:
             print("Oops! That was no valid number. Try again...")
         else:
+            tries += 1
             if guess > answer:
-                print("It is higher!")
+                print("It is lower!")
                 continue
             elif guess < answer:
-                print("It is lower!")
+                print("It is higher!")
                 continue
             elif guess == answer:
                 print("You got it! It took you {} tries.".format(tries))
@@ -79,5 +79,5 @@ if __name__ == '__main__':
     print("Welcome to the Number Guessing Game!")
     print("------------------------------------")
     print("                                    ")
-    
+
     start_game()
