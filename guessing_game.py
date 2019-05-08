f46a376f7674f3f2bf1240cb183fe16bfe5a6f79
yushinna <yuminnakao@Yumin-Nakao.local>
2019-05-08 11:13:45 +0900

    Add welcome function

--- guessing_game.py
+++ guessing_game.py
@@ -2,35 +2,12 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces.
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
 
 import random
 
 
 def start_game(highscore=999):
-    """Psuedo-code Hints
-
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
     answer = random.randint(1, 10)
     tries = 0
     highscore = highscore
@@ -73,11 +50,15 @@ def start_game(highscore=999):
                         break
             break
 
-if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
+
+def welcome():
     print("------------------------------------")
     print("Welcome to the Number Guessing Game!")
     print("------------------------------------")
     print("                                    ")
 
+
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    welcome()
     start_game()
